4d06815c1fa77ff638c980349a706398ea57d5da
Seva Podolskiy <s_podolskiy@live.com>
2024-03-04 09:03:29 -0500

    make build

---
+++ index.be76bda5.js
@@ -0,0 +1,4 @@
+// @ts-nocheck
+/* #region hide menu icon on click */var o=document.querySelector(".top-bar__icons"),e=document.querySelector(".top-bar__icon--menu"),t=document.querySelector(".top-bar__icon--close");null==e||e.addEventListener("click",function(){o.classList.toggle("top-bar__icons--show")}),null==t||t.addEventListener("click",function(){o.classList.toggle("top-bar__icons--show")});/* #end hide menu icon on click *///# sourceMappingURL=index.be76bda5.js.map
+
+//# sourceMappingURL=index.be76bda5.js.map
